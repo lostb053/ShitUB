473f8f5aeb8900797690d9c48853ed4cf2a8bf67
Lucky Jain <72547660+lostb053@users.noreply.github.com>
2021-10-16 07:37:49 +0530

    Update __init__.py

--- MinUB/__init__.py
+++ MinUB/__init__.py
@@ -1,1 +1,17 @@
-# coming soon
+import os
+from pyrogram import Client
+
+TRIGGERS = os.environ.get("TRIGGERS", "/ !").split()
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_NAME = os.environ.get("BOT_NAME")
+DB_URL = os.environ.get("DATABASE_URL")
+API_ID = int(os.environ.get("API_ID"))
+LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID"))
+OWNER = list(filter(lambda x: x, map(int, os.environ.get("OWNER_ID", "1005170481 972029825").split())))
+DOWN_PATH = "MinUB/downloads/"
+HELP_DICT = dict()
+
+plugins = dict(root="anibot/plugins")
+anibot = Client("anibot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=plugins)
+user = Client(os.environ.get('HU_USER_SESSION'), api_id=API_ID, api_hash=API_HASH)
