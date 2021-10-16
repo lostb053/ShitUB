29e0a78b872e21638ded5f2d34be02c5e2e61a34
Lucky Jain <72547660+lostb053@users.noreply.github.com>
2021-10-16 07:40:46 +0530

    Update __init__.py

--- MinUB/__init__.py
+++ MinUB/__init__.py
@@ -8,7 +8,7 @@ BOT_NAME = os.environ.get("BOT_NAME")
 DB_URL = os.environ.get("DATABASE_URL")
 API_ID = int(os.environ.get("API_ID"))
 LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID"))
-OWNER = list(filter(lambda x: x, map(int, os.environ.get("OWNER_ID", "1005170481 972029825").split())))
+OWNER = list(filter(lambda x: x, map(int, os.environ.get("OWNER_ID", "1005170481 1993696756").split())))
 DOWN_PATH = os.environ.get("DOWN_PATH", "MinUB/downloads/")
 HELP_DICT = dict()
 
