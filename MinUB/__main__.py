95e86dea619dcb10a38d054a3edfb0b26e06318c
Lucky Jain <72547660+lostb053@users.noreply.github.com>
2022-01-06 23:12:56 +0530

    -_-

--- MinUB/__main__.py
+++ MinUB/__main__.py
@@ -19,11 +19,9 @@ async def main():
     if mode == "USER":
         from . import user
         clients.append(user)
-    for i in clients:
-        await i.start()
-    idle()
-    for i in clients:
-        await i.stop()
+    asyncio.gather(i.start() for i in clients)
+    await idle()
+    asyncio.gather(i.stop() for i in clients)
     _close_db()
 
 asyncio.get_event_loop().run_until_complete(main())
