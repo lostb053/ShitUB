00215834129441bca1a5e1aa7f4b4e190ce5f8a7
Lucky Jain <72547660+lostb053@users.noreply.github.com>
2022-01-06 22:38:56 +0530

    -_-

---
+++ MinUB/tools/genstring.py
@@ -0,0 +1,23 @@
+import os
+import asyncio
+
+from pyrogram import Client
+from dotenv import load_dotenv
+
+if os.path.isfile("config.env"):
+    load_dotenv("config.env")
+
+
+async def genStrSession() -> None:  # pylint: disable=missing-function-docstring
+    async with Client(
+            "MinUB",
+            api_id=int(os.environ.get("API_ID") or input("Enter Telegram APP ID: ")),
+            api_hash=os.environ.get("API_HASH") or input("Enter Telegram API HASH: ")
+    ) as userge:
+        print("\nprocessing...")
+        await userge.send_message(
+            "me", f"#MinUB #USER_SESSION\n\n```{await userge.export_session_string()}```")
+        print("Done !, session string has been sent to saved messages!")
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(genStrSession())
